ae812da5194905815221915d189ae5429632f1db
Brittany Campbell <bacwalton@gmail.com>
2020-02-05 23:59:29 +0000

    Added new file.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - prints a specified string
+ *
+ *Description: will print the message "with proper grammar, but
+ *the outcome is a piece of art,"
+ *Return: 0 if program runs without errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
