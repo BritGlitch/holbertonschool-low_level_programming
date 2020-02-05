2a98891e486f735128e4e63d277cc0961286eb27
Brittany Campbell <bacwalton@gmail.com>
2020-02-05 23:48:18 +0000

    Fixed header description in 4-puts file.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
  *main - prints a specified string
  *
  *Description: Prints out message "Programming is like
  *building a multilingual puzzle"
- *
  *Return: 0 if program executed with no error
  */
-
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
