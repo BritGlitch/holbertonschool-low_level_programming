02c0e0e3f42c94f7d879cf526e12732e89e9b86b
Brittany Campbell <bacwalton@gmail.com>
2020-02-06 02:51:31 +0000

    Added a description header to file.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ *main - prints a specified string
+ *
+ *Description: Prints, line by line, the size of various types
+ *on the computer it is compiled and run on.
+ *Return: 0 if no errors
+ */
+
+int main(void)
 {
 	char c;
 	int i;
@@ -8,10 +16,10 @@ int main (void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof (c));
-	printf("Size of an int: %lu byte(s)\n", sizeof (i));
-	printf("Size of a long int: %lu byte(s)\n", sizeof (li));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof (lli));
-	printf("Size of a float: %lu byte(s)\n", sizeof (f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
